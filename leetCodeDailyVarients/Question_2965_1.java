12b731dbe8da4f04e28c7e8f62df188b71d162cd
piyushdhanore <piyushdhanore14@gmail.com>
2025-03-06 15:28:05 +0530

    Daily leetcode questions

--- leetCodeDailyVarients/Question_2965_1.java
+++ leetCodeDailyVarients/Question_2965_1.java
@@ -1,4 +1,19 @@
 package leetCodeDailyVarients;
 
+import java.util.Arrays;
+
 public class Question_2965_1 {
+    public static void main(String[] args) {
+//        int[][] nums = {{1,3},{2,2}};
+//        int[][] nums = {{1,2},{4,4}};
+        int[][] nums = {{9,1,7},{8,9,2},{3,4,6}};
+
+        int [] res = findMissingAndRepeatedValues(nums);
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+
+        return new int[2];
+    }
+
 }
