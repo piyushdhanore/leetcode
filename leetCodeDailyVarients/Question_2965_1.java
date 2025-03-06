98f5b0254fc8f3d14443cc68dd8f57d401286ae3
piyushdhanore <piyushdhanore14@gmail.com>
2025-03-06 16:40:08 +0530

    Daily leetcode questions

--- leetCodeDailyVarients/Question_2965_1.java
+++ leetCodeDailyVarients/Question_2965_1.java
@@ -1,6 +1,7 @@
 package leetCodeDailyVarients;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Question_2965_1 {
     public static void main(String[] args) {
@@ -13,7 +14,39 @@ public class Question_2965_1 {
     }
     public static int[] findMissingAndRepeatedValues(int[][] grid) {
 
-        return new int[2];
+        int count = 0, a,b, maxSum=0;
+
+        int[] arr = new int[2];
+
+        HashSet<Integer> set = new HashSet<>();
+
+//        for(int[] nums : grid){
+//            for (int n: nums) ct++;
+//        }
+//        System.out.println("og count: "+ct);
+
+        for (int i=0; i< grid.length; i++) {
+            for (int j = 0; j <grid[i].length; j++) {
+                if (set.contains(grid[i][j])) {
+                    a = grid[i][j];
+                } else {
+                    set.add(grid[i][j]);
+                }
+                count++;
+            }
+            maxSum += count;
+        }
+
+
+        System.out.println(count);
+        System.out.println(maxSum);
+
+        System.out.println(set);
+
+
+
+//        return new int[]{a,b};
+        return arr;
     }
 
 }
---
+++ leetCodeDailyVarients/Question_2965_2.java
@@ -0,0 +1,7 @@
+package leetCodeDailyVarients;
+
+public class Question_2965_2 {
+    public static void main(String[] args) {
+
+    }
+}
