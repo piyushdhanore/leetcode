7b3a336e3c06b7dec5ac6f20422b417ae9ea6827
piyushdhanore <piyushdhanore14@gmail.com>
2025-03-09 15:49:29 +0530

    Daily leetcode questions

--- leetCodeDaily/Question_3208.java
+++ leetCodeDaily/Question_3208.java
@@ -1,4 +1,42 @@
 package leetCodeDaily;
 
+/*
+3208. Alternating Groups II
+
+There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+
+colors[i] == 0 means that tile i is red.
+colors[i] == 1 means that tile i is blue.
+An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+
+Return the number of alternating groups.
+
+Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+
+Example 1:
+Input: colors = [0,1,0,1,0], k = 3
+Output: 3
+
+Example 2:
+Input: colors = [0,1,0,0,1,0,1], k = 6
+Output: 2
+
+Example 3:
+Input: colors = [1,1,0,1], k = 4
+Output: 0
+ */
+
 public class Question_3208 {
+    public static void main(String[] args) {
+
+        int [] colors = {0,1,0,1,0}; int k =3;
+//        int [] colors = {0,1,0,0,1,0,1}; int k =6;
+//        int [] colors = {1,1,0,1}; int k =4;
+    }
+
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+
+
+        return 0;
+    }
 }
